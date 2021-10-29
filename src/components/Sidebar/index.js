3f5fcfbd77021bbc209acd7580967b9c8c4fff2f
jesusGZ <j.gr.zurita@gmail.com>
2021-10-29 14:10:10 -0500

    Agregacion del Sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { 
+    SidebarContainer, 
+    Icon, 
+    CloseIcon, 
+    SidebarMenu, 
+    SidebarLink, 
+    SidebarRoute, 
+    SideBtnWrap 
+} from './SidebarElements'
+
+const Sidebar = ({ isOpen, toggle }) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon />
+            </Icon>
+            <SidebarMenu>
+                <SidebarLink to='/'>Pizzas</SidebarLink>
+                <SidebarLink to='/'>Pizzas</SidebarLink>
+                <SidebarLink to='/'>Todo el menu</SidebarLink>
+            </SidebarMenu>
+            <SideBtnWrap>
+                <SidebarRoute to='/'>Ordenar</SidebarRoute>
+            </SideBtnWrap>
+        </SidebarContainer>
+    )
+};
+
+export default Sidebar;
