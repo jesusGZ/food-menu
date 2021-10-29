a0f1a8789ae6f7ef0c1832694c249bbb08b0f23d
jesusGZ <j.gr.zurita@gmail.com>
2021-10-29 17:31:54 -0500

    informacion provicional

---
+++ src/components/Products/data.js
@@ -0,0 +1,63 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Pizza Suprema',
+    desc: '.................',     
+    price: '€19.99',
+    button: 'Añadir al carrito'
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Paraíso hawaiano',
+    desc: '.................',
+     price: '€16.99',
+    button: 'Añadir al carrito'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Sobrecarga de verduras',
+    desc: '.................',
+    price: '€14.99',
+    button: 'Añadir al carrito'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: 'Donas',
+    name: 'Rica dona',
+    desc:
+      'Donas glaseadas de chocolate belga cubiertas de glaseado con chispitas en la parte superior',
+    price: '€9.99',
+    button: 'Añadir al carrito'
+  },
+  {
+    img: sweet3,
+    alt: 'Helado',
+    name: 'Caramelo Maravilla',
+    desc:
+      'Vhelado de vainilla cubierto con glaseado de caramelo y chocolate cubierto con una barra de coco',
+    price: '€12.99',
+    button: 'Añadir al carrito'
+  },
+  {
+    img: sweet1,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Cuadritos de brownie de chocolate blanco cubiertos con trozos de chocolate blanco y nueces de macadamia',
+    price: ' €9.99',
+    button: 'Añadir al carrito'
+  }
+];
