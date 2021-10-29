b0a04fe670844cad20044d975060c5602ac286d4
jesusGZ <j.gr.zurita@gmail.com>
2021-10-29 17:33:28 -0500

    app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { GlobalStyle } from "./globalStyles"
-import Hero from "./components/Hero";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading='Elige tu favorito' data={productData} />
+      <Feature />
+      <Products heading='Dulces golosinas para ti' data={productDataTwo} />
+      <Footer />
     </Router>
   );
 }
