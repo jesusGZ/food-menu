41cc8fdf9fd9d2556539bfe9dba11ecfef736f3b
jesusGZ <j.gr.zurita@gmail.com>
2022-02-14 09:24:15 -0600

    corregir error de postcss

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,15 @@ module.exports = {
     node: true,
   },
   plugins: ['prettier', 'security'],
-  extends: ['google', 'prettier', 'plugin:security/recommended'],
+  extends: ['eslint:recommended', 'google', 'prettier', 'plugin:security/recommended'],
   parserOptions: {
     ecmaVersion: 2021,
   },
   rules: {
     camelcase: 0,
+    indent: ['error', 4],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
   },
 };
